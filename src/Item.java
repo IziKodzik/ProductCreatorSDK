596cc43724135d348ed234b3cadd82ad3c7e3ba8
IziKodzik <61714914+IziKodzik@users.noreply.github.com>
2021-01-20 23:57:07 +0100

    Item created

---
+++ src/Item.java
@@ -0,0 +1,27 @@
+public class Item {
+
+	private int id;
+	private int weight;
+
+
+	public Item(int id, int weight) {
+		this.id = id;
+		this.weight = weight;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+}
---
+++ src/Storage.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Storage {
+
+	List<Item> items = new ArrayList<>();
+
+
+
+}
