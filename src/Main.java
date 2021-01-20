57d6ff7e02f3d2a4a8536dcd4cd14a4aab7173db
IziKodzik <61714914+IziKodzik@users.noreply.github.com>
2021-01-21 00:04:12 +0100

    Items created

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
 
 	public static void main(String[] args) {
+		try(BufferedWriter writer = new BufferedWriter(new FileWriter("Towary.txt"))){
 
+		for (int i = 0; i < 11000; i++) {
+				writer.write(String.valueOf((int)(Math.random()*255)));
+				writer.write(' ');
+				writer.write(String.valueOf((int)(Math.random()*255)));
+				writer.write('\n');
 
-
+		}
+		}catch (IOException e){
+			e.printStackTrace();
+		}
 	}
 
 
